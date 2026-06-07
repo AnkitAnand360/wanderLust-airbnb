f9d03fde69a63729fd62f4dc48e6097a2bff0ec8
Ankit Anand <ankitanand3902@gmail.com>
2026-06-07 08:45:34 +0530

    initialized database

--- models/listing.js
+++ models/listing.js
@@ -2,15 +2,23 @@ const mongoose =require("mongoose");
 const Schema=mongoose.Schema;
 
 const listingSchema = new Schema({
-    tittle: {
+    title: {
         type: String,
         required: true,
     },
+
     description: String,
-    image: {
-        default: "https://news.airbnb.com/wp-content/uploads/sites/4/2022/11/10_Elegant-Secluded-Cabin.jpg?resize=2048,1365",
-        type: String,
-        set:(v) => v ==="" ? "https://news.airbnb.com/wp-content/uploads/sites/4/2022/11/10_Elegant-Secluded-Cabin.jpg?resize=2048,1365" : v,
+     image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+
+        url: {
+            type: String,
+            default:
+                "https://news.airbnb.com/wp-content/uploads/sites/4/2022/11/10_Elegant-Secluded-Cabin.jpg?resize=2048,1365",
+        },
     },
     price: Number, 
     location: String,
