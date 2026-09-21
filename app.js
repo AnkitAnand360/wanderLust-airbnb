9bcd3b55715d615448f8da89520c5ff8e3cd5904
Ankit Anand <ankitanand3902@gmail.com>
2026-09-21 13:34:24 +0530

    Updated MongoAtlas and Ui design

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo").default || require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,7 +22,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() =>{
     console.log("connected to DB");
@@ -30,7 +31,7 @@ main().then(() =>{
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -40,8 +41,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs" , ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", (err) => {
+    console.log("Error in MONGO STORE", err);
+});
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET, 
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -52,10 +66,12 @@ const sessionOptions = {
     },           
 };
 
-// api set
-app.get("/" , (req,res)=>{
-    res.send("Api is working");
-});
+//  api set
+// app.get("/" , (req,res)=>{
+//     res.send("Api is working");
+// });
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
